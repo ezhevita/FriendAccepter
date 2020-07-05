49f556ef353a279ba2941e871bc8e9da375a32f7
Vitaliya Chumakova <vichuma9@gmail.com>
2020-07-05 23:01:11 +0300

    Initial commit

---
+++ Vital7.FriendAccepter/FriendAccepter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Composition;
+using System.Threading.Tasks;
+using ArchiSteamFarm;
+using ArchiSteamFarm.Plugins;
+
+namespace Vital7.FriendAccepter {
+	[Export(typeof(IPlugin))]
+	public class FriendAccepter : IBotFriendRequest {
+		public void OnLoaded() {
+			ASF.ArchiLogger.LogGenericInfo(Name + " is initialized successfully!");
+		}
+
+		public string Name => nameof(FriendAccepter);
+		public Version Version => new Version(1, 0, 0, 0);
+		public Task<bool> OnBotFriendRequest(Bot bot, ulong steamID) => Task.FromResult(true);
+	}
+}
