1874415f0203a196a9c3bb844daf6f153e88ff46
Vitaliya Chumakova <vichuma9@gmail.com>
2021-01-17 04:17:00 +0300

    Cleanup, add ASF as a submodule

--- Vital7.FriendAccepter/FriendAccepter.cs
+++ Vital7.FriendAccepter/FriendAccepter.cs
@@ -1,18 +1,21 @@
 ﻿using System;
 using System.Composition;
+using System.Reflection;
 using System.Threading.Tasks;
 using ArchiSteamFarm;
 using ArchiSteamFarm.Plugins;
+using JetBrains.Annotations;
 
 namespace Vital7.FriendAccepter {
 	[Export(typeof(IPlugin))]
+	[UsedImplicitly]
 	public class FriendAccepter : IBotFriendRequest {
 		public void OnLoaded() {
 			ASF.ArchiLogger.LogGenericInfo(Name + " is initialized successfully!");
 		}
 
 		public string Name => nameof(FriendAccepter);
-		public Version Version => new Version(1, 0, 0, 0);
+		public Version Version => Assembly.GetExecutingAssembly().GetName().Version ?? throw new InvalidOperationException(nameof(Version));
 		public Task<bool> OnBotFriendRequest(Bot bot, ulong steamID) => Task.FromResult(true);
 	}
 }
